8160fbe14d7777c8252f4618d29e5a23b84599e1
Jimmi <jhested@gmail.com>
2022-09-27 21:05:29 +0200

    inital

---
+++ MyBlazerServerApp/Data/JobHandlerJobService.cs
@@ -0,0 +1,33 @@
+﻿namespace MyBlazerServerApp.Data
+{
+    public class JobHandlerJobService
+    {
+        public Task<IEnumerable<JobHandlerJob>> GetJobsAsync(string state)
+        {
+            return Task.Run(() =>
+            {
+                return Enumerable.Range(1, 3)
+                    .Select(x => new JobHandlerJob 
+                    { 
+                        Id = x, 
+                        Name = $"Job {x}", 
+                        State = state
+                    });
+            });
+        }
+
+        public Task<IEnumerable<WorkTask>> GetTasksAsync(int jobId)
+        {
+            return Task.Run(() =>
+            {
+                return Enumerable.Range(1, 50)
+                    .Select(x => new WorkTask
+                    {
+                        Id = x,
+                        Name = $"Job {x}",
+                        State = "green"
+                    });
+            });
+        }
+    }
+}
---
+++ MyBlazerServerApp/Data/WorkTask.cs
@@ -0,0 +1,11 @@
+﻿namespace MyBlazerServerApp.Data
+{
+    public class WorkTask
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? State { get; set; }
+        public string? Type { get; set; }
+        public DateTime? CreatedWhen { get; set; }
+    }
+}
